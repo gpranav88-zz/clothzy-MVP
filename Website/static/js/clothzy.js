aa12f5b3d6a75397002e2c44acb4930eeba522a5
Harsh <mail@hsinha.me>
2013-11-22 00:42:23 +0530

    Need to use defferds

--- Website/static/js/clothzy.js
+++ Website/static/js/clothzy.js
@@ -38,82 +38,90 @@
 var app = angular.module('main',[]);
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
-    $routeProvider
-        .when ('/',{
-            templateUrl:'home.html'
-        })
-        .when ('/store/:id',{
-            templateUrl:'store.html'
-        })
-        .when ('/product/:id',{
-            templateUrl:'product.html'
-        });            
+	$routeProvider
+		.when ('/',{
+			templateUrl:'home.html'
+		})
+		.when ('/store/:id',{
+			templateUrl:'store.html'
+		})
+		.when ('/product/:id',{
+			templateUrl:'product.html'
+		});            
 
    //$locationProvider
    //    .html5Mode(true)
    //    .hashPrefix('!');
 }]);
 
-app.controller('homePageController',['$scope','$http',function($scope,$http,homePageFactory){
+app.controller('homePageController',['$scope','$http','homePageFactory',function($scope,$http,homePageFactory){
 
-               $scope.featuredStores=homePageFactory.getFeaturedStores($http);
 
-               $scope.latestDesigns=homePageFactory.getLatestDesigns($http);
+		
+			//$scope.featuredStores=homePageFactory.getDummy();
 
-               $scope.topReviewers=homePageFactory.getTopReviewers($http);
+			$scope.featuredStores=homePageFactory.getFeaturedStores($http);
 
-           }            
+			$scope.latestDesigns=homePageFactory.getLatestDesigns($http);
+
+			$scope.topReviewers=homePageFactory.getTopReviewers($http);
+
+		   }            
    ]);
 
 app.factory('homePageFactory',function(){
 
-    return {
+	return {
 
-        getFeaturedStores:function($http){
+		getFeaturedStores:function($http){
 
-            $http({
-                method: 'GET', url: '/api/store/featured/'
-            })
-            .success(function(data, status, headers, config) {
-               return data;
+			//console.log('wolololo');
 
-           });
-
-        },
-
+			$http({
+				method: 'GET', url: '/api/store/featured/'
+			})
+			.success(function(data, status, headers, config) {
+			console.log(data);
+			   return data;
 
 
-        getLatestDesigns:function($http){
+		   });
 
-            $http({
-                method: 'GET', url: '/api/product/latest/'
-            })
-            .success(function(data, status, headers, config) {
-               return data;
-
-           });
-
-        },
-
-
-        getTopReviewers:function($http){
-
-            $http({
-                method: 'GET', url: '/api/user/top/'
-            })
-            .success(function(data, status, headers, config) {
-               return data;
-
-           });
-
-        },
+		},
 
 
 
+		getLatestDesigns:function($http){
+
+			$http({
+				method: 'GET', url: '/api/product/latest/'
+			})
+			.success(function(data, status, headers, config) {
+			   return data;
+
+		   });
+
+		},
+
+
+		getTopReviewers:function($http){
+
+			$http({
+				method: 'GET', url: '/api/user/top/'
+			})
+			.success(function(data, status, headers, config) {
+			   return data;
+
+		   });
+
+		}
 
 
 
-    };
+
+
+
+	};
 
 
 
