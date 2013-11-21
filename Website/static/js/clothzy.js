3c0e8ba0634f77c0836229218b9f56e7c96e2562
Harsh <mail@hsinha.me>
2013-11-21 20:16:46 +0530

    Async issues with AJAX

--- Website/static/js/clothzy.js
+++ Website/static/js/clothzy.js
@@ -1,36 +1,121 @@
-angular
-    .module('main',[])
-    .config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
-        $routeProvider
-            .when ('/',{
-                templateUrl:'home.html'
-            })
-            .when ('/store/:id',{
-                templateUrl:'store.html'
-            })
-            .when ('/product/:id',{
-                templateUrl:'product.html'
-            });            
+//angular
+//    .module('main',[])
+//    .config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
+//        $routeProvider
+//            .when ('/',{
+//                templateUrl:'home.html'
+//            })
+//            .when ('/store/:id',{
+//                templateUrl:'store.html'
+//            })
+//            .when ('/product/:id',{
+//                templateUrl:'product.html'
+//            });            
+////
+//        //$locationProvider
+//        //    .html5Mode(true)
+//        //    .hashPrefix('!');
+//    }])
+//    .controller('home',['$scope','$http',function($scope,$http){
+//        $http({method: 'GET', url: '/api/store/featured/'})
+//            .success(function(data, status, headers, config) {
+//                $scope.featuredStores=data;
 //
-        //$locationProvider
-        //    .html5Mode(true)
-        //    .hashPrefix('!');
-    }])
-    .controller('home',['$scope','$http',function($scope,$http){
-        $http({method: 'GET', url: '/api/store/featured/'})
+//            });
+//        $http({method: 'GET', url: '/api/product/latest/'})
+//            .success(function(data, status, headers, config) {
+//                $scope.latestDesigns=data;
+//
+//            });
+//        $http({method: 'GET', url: '/api/user/top/'})
+//            .success(function(data, status, headers, config) {
+//                $scope.topReviewers=data;
+//
+//            });            
+//    }]);
+
+
+var app = angular.module('main',[]);
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
+    $routeProvider
+        .when ('/',{
+            templateUrl:'home.html'
+        })
+        .when ('/store/:id',{
+            templateUrl:'store.html'
+        })
+        .when ('/product/:id',{
+            templateUrl:'product.html'
+        });            
+
+   //$locationProvider
+   //    .html5Mode(true)
+   //    .hashPrefix('!');
+}]);
+
+app.controller('homePageController',['$scope','$http',function($scope,$http,homePageFactory){
+
+               $scope.featuredStores=homePageFactory.getFeaturedStores($http);
+
+               $scope.latestDesigns=homePageFactory.getLatestDesigns($http);
+
+               $scope.topReviewers=homePageFactory.getTopReviewers($http);
+
+           }            
+   ]);
+
+app.factory('homePageFactory',function(){
+
+    return {
+
+        getFeaturedStores:function($http){
+
+            $http({
+                method: 'GET', url: '/api/store/featured/'
+            })
             .success(function(data, status, headers, config) {
-                $scope.featuredStores=data;
+               return data;
 
-            });
-        $http({method: 'GET', url: '/api/product/latest/'})
+           });
+
+        },
+
+
+
+        getLatestDesigns:function($http){
+
+            $http({
+                method: 'GET', url: '/api/product/latest/'
+            })
             .success(function(data, status, headers, config) {
-                $scope.latestDesigns=data;
+               return data;
 
-            });
-        $http({method: 'GET', url: '/api/user/top/'})
+           });
+
+        },
+
+
+        getTopReviewers:function($http){
+
+            $http({
+                method: 'GET', url: '/api/user/top/'
+            })
             .success(function(data, status, headers, config) {
-                $scope.topReviewers=data;
+               return data;
 
-            });            
-    }]);
+           });
 
+        },
+
+
+
+
+
+
+    };
+
+
+
+
+});
